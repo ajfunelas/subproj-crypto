26512d3aab21e4bf45ef96346e04cb26537847d0
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2019-11-01 18:28:26 +0800

    signIn Invocation success
    - changed signIn and signUp url
    - /api/signIn & /api/signUp

--- httpHandlers.go
+++ httpHandlers.go
@@ -10,15 +10,30 @@ import (
 	// "net/http"
 	// "encoding/json"
 	
-    "github.com/go-chi/chi"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
     uuid "github.com/gofrs/uuid"
 )
 func (dbDriver *DbDriver) startRoutes() {
-    r := getRouter()
+	r := chi.NewRouter()
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	  })
+	  r.Use(cors.Handler)
+
     r.Post("/api/favourites/list", dbDriver.getFaves)
 	r.Post("/api/favourites/toggle", dbDriver.tglFave)
-	r.Post("/api/favourites/signin", dbDriver.signinHandler)
-	r.Post("/api/favourites/signup", dbDriver.signUpHandler)
+	r.Post("/api/signin", dbDriver.signinHandler)
+	r.Post("/api/signup", dbDriver.signUpHandler)
+
+	
 	http.ListenAndServe(":8080", r)
 	
 }
